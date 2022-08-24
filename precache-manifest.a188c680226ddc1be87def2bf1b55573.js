e918933f1a3100b269c76b4e73bed87eb093be0e
sky <hcomeon@gmail.com>
2022-08-24 14:42:31 +0800

    deploy

---
+++ precache-manifest.a188c680226ddc1be87def2bf1b55573.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd3ea88d2eb88a495a51",
+    "url": "/guoguo-1-website/css/app.eec6772a.css"
+  },
+  {
+    "revision": "71fb0fb98e5972be5b9898142a4b3e5d",
+    "url": "/guoguo-1-website/index.html"
+  },
+  {
+    "revision": "cd3ea88d2eb88a495a51",
+    "url": "/guoguo-1-website/js/app.bc182742.js"
+  },
+  {
+    "revision": "8a57ada3e212e3aafebd",
+    "url": "/guoguo-1-website/js/chunk-vendors.fcd555e1.js"
+  },
+  {
+    "revision": "bc671308298f9d7977c9fc94ea5cee94",
+    "url": "/guoguo-1-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/guoguo-1-website/robots.txt"
+  }
+]);
